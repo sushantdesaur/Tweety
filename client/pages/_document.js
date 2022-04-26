b61f370764a679fa8b14fd5907b2d9ee2a634d53
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-26 17:48:45 +0530

    Basic Project Structure

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -5,20 +5,29 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
-        ></link>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
-        ></script>
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          src="https://unpkg.com/react/umd/react.production.min.js"
+          crossorigin="anonymous"
+        ></script>
+
+        <script
+          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+          crossorigin="anonymous"
+        ></script>
+
+        <script
+          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossorigin="anonymous"
+        ></script>
       </body>
     </Html>
   );

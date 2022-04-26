b80fae25ef6570e0d5e81ac7b7b30d0f6e4f94ea
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-26 16:33:54 +0530

    Setted up express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Initialize the express app
+const app = express();
+
+// Port Number
+const port = 5000;
+
+const server = app.listen(port, () => {
+   console.log(`Server is running on port ${port}`); 
+});
+
+
